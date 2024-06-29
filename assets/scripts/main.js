069406c08d8791c5d7be6f5e73fa1439f4bb16ab
annavanwingerden <91270891+annavanwingerden@users.noreply.github.com>
2024-06-29 14:27:46 +0100

    init

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,8 +6,10 @@ class Game {
         this.height = this.canvas.height;
         this.baseHeight = 720;
         this.ratio = this.height / this.baseHeight; 
+        this.background = new Background(this);
         this.player = new Player(this);
         this.gravity;
+        this.speed;
 
         this.resize(window.innerWidth, window.innerHeight);
 
@@ -35,11 +37,14 @@ class Game {
             this.height= this.canvas.height;
             this.ratio = this.height / this.baseHeight; 
 
-            this.gravity = 0.15 *this.ratio;
+            this.gravity = 0.15 * this.ratio;
+            this.speed = 2 * this.ratio;
+            this.background.resize();
             this.player.resize();    
     }
     render (){
-        //this.ctx.fillStyle = 'red';
+        this.background.update();
+        this.background.draw();
         this.player.update();
         this.player.draw();
     }
