dddd7eeda4ab844eb9b478ea7053fd4bff887b73
annavanwingerden <91270891+annavanwingerden@users.noreply.github.com>
2024-06-29 09:42:30 +0100

    init

---
+++ assets/scripts/background.js
@@ -0,0 +1,8 @@
+class Background {
+    constructor(game){
+        this.game = game;
+        this.image = document.getElementById('background');
+        this.width = 2400;
+        this.height = 720;
+    }
+}
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,10 +4,42 @@ class Game {
         this.ctx = context;
         this.width = this.canvas.width;
         this.height = this.canvas.height;
+        this.baseHeight = 720;
+        this.ratio = this.height / this.baseHeight; 
         this.player = new Player(this);
+        this.gravity;
+
+        this.resize(window.innerWidth, window.innerHeight);
+
+        window.addEventListener('resize',  e => {
+            this.resize(e.currentTarget.innerWidth, e.currentTarget.innerHeight);
+        });
+        // mouse controls
+        this.canvas.addEventListener('mousedown', e => {
+            this.player.flap();
+        });
+        // keyboard controls
+        window.addEventListener('keydown', e => {
+            if (e.key === ' ' || e.key === 'Enter') this.player.flap();
+        });
+        // touch controls
+        this.canvas.addEventListener('touchstart', e => {
+            this.player.flap();
+        });
+    }
+    resize (width, height){
+        this.canvas.width= width;
+            this.canvas.height= height;
+            this.ctx.fillStyle = 'red';
+            this.width = this.canvas.width;
+            this.height= this.canvas.height;
+            this.ratio = this.height / this.baseHeight; 
+
+            this.gravity = 0.15 *this.ratio;
+            this.player.resize();    
     }
     render (){
-        this.ctx.fillStyle = 'red';
+        //this.ctx.fillStyle = 'red';
         this.player.update();
         this.player.draw();
     }
@@ -17,7 +49,7 @@ window.addEventListener('load', function(){
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
     canvas.width = 720;
-    canvas.height = 720; 
+    canvas.height = 720;
 
     const game = new Game(canvas, ctx);
     game.render();
