02a91cbf5b496448e1dff571d863af1f90de4a4b
annavanwingerden <91270891+annavanwingerden@users.noreply.github.com>
2024-07-23 15:56:30 +0100

    commit

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -14,6 +14,7 @@ class Game {
         this.speed;
         this.score;
         this.gameOver;
+        this.timer;
 
         this.resize(window.innerWidth, window.innerHeight);
 
@@ -35,26 +36,28 @@ class Game {
     }
     resize (width, height){
         this.canvas.width= width;
-            this.canvas.height= height;
-            this.ctx.fillStyle = 'blue';
-            this.ctx.font = '30px Arsenal SC';
-            this.ctx.textAlign='right';
-            this.width = this.canvas.width;
-            this.height= this.canvas.height;
-            this.ratio = this.height / this.baseHeight; 
+        this.canvas.height= height;
+        this.ctx.fillStyle = 'blue';
+        this.ctx.font = '30px Arsenal SC';
+        this.ctx.textAlign='right';
+        this.width = this.canvas.width;
+        this.height= this.canvas.height;
+        this.ratio = this.height / this.baseHeight; 
 
-            this.gravity = 0.15 * this.ratio;
-            this.speed = 2 * this.ratio;
-            this.background.resize();
-            this.player.resize();    
-            this.createObstacles();
-            this.obstacles.forEach (obstacle => {
-                obstacle.resize();
-            });
-            this.score = 0;
-            this.gameOver = false; 
+        this.gravity = 0.15 * this.ratio;
+        this.speed = 2 * this.ratio;
+        this.background.resize();
+        this.player.resize();    
+        this.createObstacles();
+        this.obstacles.forEach (obstacle => {
+            obstacle.resize();
+        });
+        this.score = 0;
+        this.gameOver = false; 
+        this.timer = 0;
     }
-    render (){
+    render (deltaTime){
+       if (!this.gameOver) this.timer += deltaTime;
         this.background.update();
         this.background.draw();
         this.drawStatusText();
@@ -73,9 +76,19 @@ class Game {
             this.obstacles.push(new Obstacle(this, firstX + i * obstacleSpacing));
         }
     }
+    formatTimer (){
+        return (this.timer * 0.001).toFixed(1);
+    }
     drawStatusText(){
         this.ctx.save();
         this.ctx.fillText('Score: ' + this.score, this.width - 10, 30);
+        this.ctx.textAlign = 'left';
+        this.ctx.fillText('Timer: ' + this.formatTimer(), 10, 30);
+        if (this.gameOver){
+            this.ctx.textAlign = 'center';
+            this.ctx.font = '30px Bungee';
+            this.ctx.FillText('GAME OVER', this.width * 0.5, this.height *0.5)
+        }
         this.ctx.restore();
     }
 }
@@ -89,10 +102,13 @@ window.addEventListener('load', function(){
     const game = new Game(canvas, ctx);
     game.render();
 
-    function animate (){
+    let lastTime = 0;
+    function animate (timeStamp){
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
         ctx.clearRect(0,0, canvas.width, canvas.height);
-        game.render();
-        if(!game.gameOver) requestAnimationFrame(animate);
+        game.render(deltaTime);
+       requestAnimationFrame(animate);
     }
     
     requestAnimationFrame(animate);
